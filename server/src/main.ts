c3f23801240b4706465175ec8aff83a5a886e8d5
chenxiang <chenxiangbj@kanyun.com>
2025-09-26 18:57:57 +0800

    feat: set global prefix

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
 
   // 启用CORS
   app.enableCors({
